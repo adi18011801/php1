c67be101610ec43af54dc3aa8758200ce6763a5c
adi18011801 <adiwakeem18@gmail.com>
2026-01-15 21:49:30 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Time Management</title>
+    <!--link to CSS file in the css folder-->
+    <link rel="stylesheet" href="./css/style.css">
+    <style>
+        a {
+            font-size: 150%;
+            color: white;
+          
+        }
+
+        body {
+            text-align: center;
+            font-size: larger;
+            outline-color: darkblue;
+        }
+        h4{
+            color: aliceblue;
+        }
+        img{
+            width: 40%;
+            height: 40%;
+        }
+      
+      
+    </style>
+
+
+</head>
+
+<body>
+    <h2> Hi there!</h2>
+    <h1> Welcome to the Time Management Website</h1>
+    <h3>to log in or sign up press the links below </h3>
+    
+    <button class="button button1">
+
+        <a href="./web-pages/login.html"> log in!</a>
+    </button>
+    <h2> or </h2>
+    <button class="button button1">
+
+        <a href="./web-pages/signup.html">sign up!</a>
+   
+ </button>
+ 
+    <h4>Learn more about this website!</h4>
+    <h3>This website helps people of all ages manage their time</h3>
+  <h3> the website gives you a calendar of all months of the year ,and every day you arrange the time as you like,</h3>
+  <h3> every hour you type the things you have to do and website saves the information you put in  </h3>
+   <h3> , and you can use this website like a calendar that organizes your time.</h3>
+
+
+   <img src="./images/hjjjj.jpg">
+</body>
+
+</html>
